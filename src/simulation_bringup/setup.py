0c1ae6c4fec8a82e18e74d5f819be9e23323e503
Wiktor Bajor <wiktorbajor1@gmail.com>
2024-06-13 22:59:00 +0200

    Add initial version of tracked urdf

--- src/simulation_bringup/setup.py
+++ src/simulation_bringup/setup.py
@@ -13,7 +13,8 @@ setup(
         ('share/' + package_name, ['package.xml']),
         (os.path.join("share", package_name, "launch"), glob("launch/*.py")),
         (os.path.join("share", package_name, "config"), glob("config/*.yaml")),
-        (os.path.join("share", package_name, "worlds"), glob("worlds/*.sdf"))
+        (os.path.join("share", package_name, "worlds"), glob("worlds/*.sdf")),
+        (os.path.join("share", package_name, "urdf"), glob("urdf/*.urdf"))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
